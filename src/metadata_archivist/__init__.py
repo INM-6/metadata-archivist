1556f82b131145d7a9ca37e0760ba576cfb32482
kelbling <matthias.kelbling@ufz.de>
2022-12-02 09:49:11 +0100

    added a first draft of a framing object

--- src/metadata_archivist/__init__.py
+++ src/metadata_archivist/__init__.py
@@ -3,3 +3,4 @@ metadata_archivist
 '''
 
 from . import decompressing_procedures, decompressor, exporter, exporting_procedures, parser, parsing_procedures
+from .archivist import Archivist
---
+++ src/metadata_archivist/archivist.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Metadata archive integrating class.
+Author: Kelbling, M.
+
+"""
+
+import json
+import sys
+import importlib
+
+from pathlib import Path
+from enum import Enum
+from typing import Optional, Union
+
+from . import decompressor as dc
+from . import parser as pr
+from . import exporter as ex
+
+
+class Archivist():
+
+    def __init__(self,
+                 config: Union[dict, str],
+                 archive: str,
+                 verbose: Optional[bool] = True,
+                 rm_dc_dir: Optional[bool] = False):
+        """init
+
+        :param config: path to config file as str or config as dict
+        :param archive: path to archive (str)
+        :param verbose: print verbose information (bool)
+        :returns: None
+
+        """
+        self.verbose = verbose
+
+        if self.verbose:
+            print('''\n----- Archivist -----''')
+
+        # get config
+        if isinstance(config, str):
+            self._load_config(config)
+        elif isinstance(config, dict):
+            self.config = config
+
+        # check config
+        self._check_config()
+
+        # check and get paths and types
+        self.archive_path, self.archive_type = dc.check_archive(archive)
+        self.dc_dir_path = self._check_dir(self.config["extraction_directory"],
+                                           allow_existing=False)
+        self.out_dir_path = self._check_dir(self.config["output_directory"],
+                                            allow_existing=True)
+        self.checked_format = ex.check_format(self.config["output_format"])
+
+        self.members = self.config["extraction_members"]
+        assert self.members is None or (isinstance(self.members, list) and all(
+            isinstance(em, str) for em in self.members))
+
+        self.mode = self.config["parsing_rules"]["mode"].lower()
+
+        if self.mode == "standard":
+            self.module_path = None
+        else:
+            assert self.mode == "overwrite"
+            assert isinstance(self.config["parsing_rules"]["module"], str)
+            self.module_path = Path(self.config["parsing_rules"]["module"])
+            assert self.module_path.is_file(
+            ) and self.module_path.suffix == ".py"
+            # TODO: implement checksum for security check against malicious code
+
+        self.rm_dc_dir = rm_dc_dir
+
+    def _check_config(self):
+        """
+        check if configuration contains required information
+        if not exit
+
+        :returns: None
+
+        """
+        req_config_keys_and_types = {
+            "extraction_directory": str,
+            "output_directory": str,
+            "output_format": str,
+            "metdata": str,
+            "extraction_members": list,
+            "parsing_rules": dict
+        }
+        if self.verbose:
+            print('Config:')
+        for kk, vv in req_config_keys_and_types.items():
+            if kk not in self.config.keys():
+                print(f'The config is expected to contain the key: {kk}')
+                sys.exit()
+            if not isinstance(self.config[kk], vv):
+                print(
+                    f'The value of {kk} is expected to be of type {vv.__name__}'
+                )
+                sys.exit()
+            if self.verbose:
+                print(f'    {kk}: {self.config[kk].__str__()}')
+
+    def _load_config(self, config_path: str):
+        """
+        Checks path to configuration file and attempts to load it.
+
+        Args:
+            config_path: String path to configuration file.
+
+        Returns:
+            None
+        """
+
+        path = Path(config_path)
+
+        assert path.is_file and path.suffix == ".json"
+
+        with path.open() as f:
+            self.config = json.load(f)
+
+    def _check_dir(self, dir_path: str, allow_existing: bool = False) -> Path:
+        """
+        Checks directory path.
+        If a directory with the same name already exists then continue.
+        If a directory in the specified path cannot be created then execution is
+        stopped.
+
+        Args:
+            dir_path: String path to output directory.
+            allow_existing: Control boolean to allow the use of existing folders.
+
+        Returns:
+            Path object to output directory.
+        """
+
+        path = Path(dir_path)
+
+        if dir_path != "":
+            exists = path.exists()
+            if not allow_existing:
+                assert not exists, f"Directory already exists: {dir_path}"
+            elif exists:
+                assert path.is_dir(), f"Incorrect path to directory: {path}"
+                return path
+
+            try:
+                path.mkdir()
+            except FileNotFoundError as e:
+                # This exception is raised if a nested path is given and intermediate directories do not exist
+                print(f"Incorrect path to directory: {e.args}")
+                sys.exit()
+
+        return path
+
+    def extract(self):
+        if self.verbose:
+            print(f'''
+Extracting:
+Archive path: {self.archive_path}
+Output path: {self.out_dir_path}
+Extraction path: {self.dc_dir_path}
+Remove extracted: {self.rm_dc_dir}''')
+
+        dc.decompress(self.archive_path,
+                      self.archive_type,
+                      self.dc_dir_path,
+                      self.members,
+                      verb=self.verbose)
+
+        if self.verbose:
+            print("Finished extracting self.archive.")
+
+        arch_name = self.archive_path.stem.split(".")[0]
+        dc_arch_dir = self.dc_dir_path.joinpath(arch_name)
+
+        if self.verbose:
+            print(f'''Extracted self.archive path: {dc_arch_dir}
+    Beginning collecting metadata.''')
+
+        module = None
+        if self.mode == "overwrite":
+            # Taken from https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+            spec = importlib.util.spec_from_file_location(
+                self.module_path.name, self.module_path)
+            module = importlib.util.module_from_spec(spec)
+            sys.modules[self.module_path.name] = module
+            spec.loader.exec_module(module)
+
+        metadata = {"_id": arch_name}
+        pr.parse_data(dc_arch_dir,
+                      metadata,
+                      self.mode,
+                      module,
+                      rm=self.rm_dc_dir,
+                      verb=self.verbose)
+
+        if self.verbose:
+            print("Finished collecting metadata.")
+
+        if self.rm_dc_dir and str(self.dc_dir) != ".":
+            self.dc_dir.rmdir()
+
+        metadata_file = self.out_dir_path.joinpath(f"{arch_name}.json")
+        ex.export(metadata,
+                  metadata_file,
+                  self.config['output_format'],
+                  verb=self.verbose)

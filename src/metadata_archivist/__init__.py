bde150107a2f3e8b0ea175068378540cb1083576
kelbling <matthias.kelbling@ufz.de>
2022-12-05 12:26:12 +0100

    renamed Archivist file to match pattern

--- src/metadata_archivist/__init__.py
+++ src/metadata_archivist/__init__.py
@@ -3,4 +3,4 @@ metadata_archivist
 '''
 
 from . import decompressing_procedures, decompressor, exporter, exporting_procedures, parser, parsing_procedures
-from .archivist import Archivist
+from .Archivist import Archivist

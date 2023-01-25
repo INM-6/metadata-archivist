a567ca7a53255a0a2897c9bec686e9614ed721e6
kelbling <matthias.kelbling@ufz.de>
2023-01-25 10:42:27 +0100

    namelist example

--- examples/namelists/main.py
+++ examples/namelists/main.py
@@ -8,81 +8,12 @@ Author: Jose V.
 
 """
 
-from metadata_archivist import Archivist, AExtractor, Parser
+from metadata_archivist import Archivist
+from my_parser import my_parser
 from pathlib import Path
-import f90nml
-import json
-
-
-class nml_extractor(AExtractor):
-
-    def __init__(self):
-        self.name = 'nml_extractor'
-        self._input_file_pattern = '*.nml'
-        self._extracted_metadata = {}
-
-        with open('nml_schema.json') as f:
-            nml_schema = json.load(f)
-
-        self._schema = nml_schema
-
-    def extract(self, data):
-        parser = f90nml.Parser()
-        a_nml = parser.reads(data.read())
-        nml_dict = {}
-
-        for nml_name, nml in a_nml.items():
-            nml_dict[nml_name] = nml.todict()
-
-        return nml_dict
-
-
-def head_rest_split_line(line: str,
-                         head_index: int = 0,
-                         split_val: str = ":",
-                         clean=None) -> dict:
-    if clean is not None:
-        line = clean(line)
-
-    line_split = line.split(split_val)
-    rest_start = head_index + 1
-    rest = line_split[rest_start:]
-    last_index = len(rest) - 1
-
-    return {
-        line_split[head_index].strip():
-        str().join([
-            i.strip() + (" " if c < last_index else "")
-            for c, i in enumerate(rest)
-        ])
-    }
-
-
-class meminfo_extractor(AExtractor):
-
-    def __init__(self):
-        self.name = 'meminfo_extractor'
-        self._input_file_pattern = 'meminfo.out'
-        self._extracted_metadata = {}
-
-        with open('mem_schema.json') as f:
-            mem_schema = json.load(f)
-
-        self._schema = mem_schema
-
-    def extract(self, f):
-        out = {}
-        for line in f:
-            if line != "\n":
-                out.update(
-                    head_rest_split_line(line, split_val=":", clean=None))
-        return out
-
-
-my_parser = Parser(extractors=[nml_extractor(), meminfo_extractor()])
 
 arch = Archivist(config='config.json',
-                 archive=Path('test_namelist.tgz'),
+                 archive=Path('metadata_archive.tar'),
                  parser=my_parser)
 
 arch.extract()
---
+++ examples/namelists/my_parser.py
@@ -0,0 +1,27 @@
+from metadata_archivist import AExtractor, Parser
+import f90nml
+
+NML_SCHEMA = {}
+
+
+class nml_extractor(AExtractor):
+
+    def __init__(self):
+        self.name = 'nml_extractor'
+        self._input_file_pattern = '*.nml'
+        self._extracted_metadata = {}
+
+        self._schema = NML_SCHEMA
+
+    def extract(self, f):
+        nml = f90nml.read(f)
+        return nml.todict()
+
+
+my_parser = Parser(extractors=[nml_extractor()])
+
+# xx = nml_extractor()
+
+# with open('metadata_archive/mhm.nml') as ff:
+#     yy = xx.extract(ff)
+# print(yy)

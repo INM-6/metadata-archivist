b9fb2a5c2fe3150e2c4146cf786d0827e0b15953
JoseJVS <villamar_jose@hotmail.com>
2023-11-23 19:08:16 +0100

    First example of calculation directive

---
+++ examples/schema_example4/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from metadata_archivist import Archivist
+from my_parser import my_parser
+from pathlib import Path
+from json import dumps
+
+arch = Archivist(path=Path('raw_metadata'),
+                 formatter=my_parser,
+                 output_directory="./",
+                 output_file="metadata.json",
+                 overwrite=True,
+                 auto_cleanup=True,
+                 verbose='debug',
+                 add_description=True,
+                 add_type=True)
+
+arch.parse()
+arch.export()
+
+print("\nResulting schema:")
+print(dumps(my_parser.schema, indent=4))
+
+print("\nResulting metadata:")
+print(dumps(arch.get_metadata(), indent=4))
---
+++ examples/schema_example4/my_parser.py
@@ -0,0 +1,147 @@
+from metadata_archivist import AParser, Formatter
+import yaml
+
+
+def key_val_split(string, split_char):
+    string = string.strip()
+    out = string.split(split_char)
+    return {out[0].strip(): out[1].strip()}
+
+
+class time_parser(AParser):
+
+    def __init__(self) -> None:
+        super().__init__(
+            name='time_parser',
+            input_file_pattern='time\.txt',
+            schema={
+                'type': 'object',
+                'properties': {
+                    'real': {
+                        'type': 'string',
+                        'description':
+                        'the time from start to finish of the call'
+                    },
+                    'user': {
+                        'type': 'string',
+                        'description': 'amount of CPU time spent in user mode'
+                    },
+                    'sys': {
+                        'type': 'string',
+                        'description':
+                        'amount of CPU time spent in kernel mode'
+                    },
+                    'system': {
+                        '$ref': '#/properties/sys'
+                    }
+                }
+            })
+
+    def parse(self, file_path) -> dict:
+        out = {}
+        with file_path.open("r") as fp:
+            for line in fp:
+                if line != '\n':
+                    out.update(key_val_split(line, '\t'))
+        return out
+
+
+class yml_parser(AParser):
+
+    def __init__(self) -> None:
+        super().__init__(name='yml_parser',
+                         input_file_pattern='.*\.yml',
+                         schema={
+                            'type': 'object',
+                            'properties': {
+                                'parameters': {
+                                    'type': 'object',
+                                    'properties': {
+                                        'sim_time': {
+                                            'type': 'number',
+                                            'description': 'total time to simulate'
+                                        },
+                                        'scale': {
+                                            'type': 'number',
+                                            'description': 'model scale'
+                                        },
+                                        'num_procs': {
+                                            'type': 'number',
+                                            'description': 'number of MPI processes'
+                                        },
+                                        'threads_per_proc': {
+                                            'type': 'number',
+                                            'description': 'number of threads used per MPI process'
+                                        },
+                                        'step_size': {
+                                            'type': 'number',
+                                            'description': 'step size for advancing simulation'
+                                        }
+                                    }
+                                }
+                            }
+                         })
+
+    def parse(self, file_path):
+        with open(file_path, "r") as stream:
+            try:
+                out = yaml.safe_load(stream)
+                return out
+            except yaml.YAMLError as exc:
+                print(exc)
+
+
+my_schema = {
+    '$schema': 'https://abc',
+    '$id': 'https://abc.json',
+    'description': 'my example schema',
+    'type': 'object',
+    'properties': {
+        'metadata_archive': {
+            'type': 'object',
+            'properties': {
+                'program_execution': {
+                    'type': 'object',
+                    'properties': {
+                        'real_time_factor': {
+                            'type': 'number',
+                            'description': 'ratio of wall clock time to simulation time',
+                            '!calculate': {
+                                'expression': '{val1} / {val2}',
+                                'variables': {
+                                    'val1': {
+                                        '!parsing': {
+                                            'keys': ['real']
+                                        },
+                                        '$ref': '#/$defs/time_parser'
+                                    },
+                                    'val2': {
+                                        '!parsing': {
+                                            'keys': ['parameters/sim_time']
+                                        },
+                                        '$ref': '#/$defs/yml_parser'
+                                    }
+                                }
+                            }
+                        },
+                        'model': {
+                            'type': 'object',
+                            'properties': {
+                                'scale': {
+                                    '!parsing': {
+                                        'keys': ['parameters/scale']
+                                    },
+                                    '$ref': '#/$defs/yml_parser'
+                                }
+                            }
+                        },
+                    }
+                },
+            },
+        },
+    }
+}
+
+my_parser = Formatter(parsers=[time_parser(),
+                               yml_parser()],
+                   schema=my_schema)

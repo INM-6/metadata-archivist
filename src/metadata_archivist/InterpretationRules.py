344f1f88aa9b9fb32d430966f015cdefb99aec31
JoseJVS <villamar_jose@hotmail.com>
2023-11-23 19:06:28 +0100

    Appended _rule suffix to respective functions

--- src/metadata_archivist/InterpretationRules.py
+++ src/metadata_archivist/InterpretationRules.py
@@ -23,38 +23,24 @@ _KNOWN_REFS = [
     "#/$defs/",
 ]
 
-def _process_simple_property(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _process_simple_property_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
     # Known simple properties return recursion results without branching
     return interpreter._interpret_schema(prop_val, parent_key, entry)
 
-def _process_pattern_property(interpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _process_pattern_property_rule(interpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
     # We create a regex context and recurse over the contents of the property.
     entry.context.update({"useRegex": True})
 
     return interpreter._interpret_schema(prop_val, parent_key, entry)
 
-def _process_parsing_directive(interpreter: SchemaInterpreter, prop_val: Union[str, dict], prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _process_parsing_directive_rule(interpreter: SchemaInterpreter, prop_val: Union[str, dict], prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
     # We create an !parsing context but keep on with current recursion level
     # Contents of this dictionary are not supposed to be handled by the interpreter.
     entry.context.update({prop_key: prop_val})
 
     return entry 
 
-def _process_reference(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
-    # Check if reference is well formed against knowledge base
-    if not any(prop_val.startswith(ss) for ss in _KNOWN_REFS):
-        raise RuntimeError(f"Malformed reference prop_value: {prop_key}: {prop_val}")
-    
-    # Get schema definitions
-    defs = interpreter._schema["$defs"]
-
-    # Call _process_refs to check for correctness before creating parsing context.
-    entry = _process_refs(defs, prop_val, entry)
-
-    return entry
-    
-
-def _process_varname(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _process_varname_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
     # Check if regex context is present in current entry
     if "useRegex" not in entry.context:
         raise RuntimeError("Contextless !varname found")
@@ -64,15 +50,20 @@ def _process_varname(interpreter: SchemaInterpreter, prop_val: dict, prop_key: s
     
     return entry
     
-def _recurse_sub_schema(sub_schema: dict, entry: SchemaEntry, filters: Optional[list] = None) -> list:
-    # TODO: recursively explore Parser sub-schema and collect entry names and description.
-    # List of keys can be passed as filters to select a branch of the Parser sub schema.
-    # Resulting list should contain all the #/property that needs to be linked
-    # with their respective positions so that _process_refs can take care of creating links
-    # and avoid circular dependencies.
-    return []
+def _process_reference_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+    # Check if reference is well formed against knowledge base
+    if not any(prop_val.startswith(ss) for ss in _KNOWN_REFS):
+        raise RuntimeError(f"Malformed reference prop_value: {prop_key}: {prop_val}")
+    
+    # Get schema definitions
+    defs = interpreter._schema["$defs"]
 
-def _process_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> SchemaEntry:
+    # Call _process_refs to check for correctness before creating parsing context.
+    entry = __process_refs(defs, prop_val, entry)
+
+    return entry
+
+def __process_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> SchemaEntry:
     # Test correctness of reference
     val_split = prop_val.split("/")
     if len(val_split) < 3:
@@ -89,17 +80,25 @@ def _process_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> Schem
 
     # Further process reference e.g. filters, internal property references -> links
     sub_schema = definitions[pid]["properties"]
-    links = _recurse_sub_schema(sub_schema, entry, filters=None if len(val_split) <= 3 else val_split[3:])
+    links = __recurse_sub_schema(sub_schema, entry, filters=None if len(val_split) <= 3 else val_split[3:])
     # TODO: take care of linking
 
     return entry
 
+def __recurse_sub_schema(sub_schema: dict, entry: SchemaEntry, filters: Optional[list] = None) -> list:
+    # TODO: recursively explore Parser sub-schema and collect entry names and description.
+    # List of keys can be passed as filters to select a branch of the Parser sub schema.
+    # Resulting list should contain all the #/property that needs to be linked
+    # with their respective positions so that _process_refs can take care of creating links
+    # and avoid circular dependencies.
+    return []
+
 _INTERPRETATION_RULES = {
-    "properties": _process_simple_property,
-    "unevaluatedProperties": _process_simple_property,
-    "additionalProperties": _process_simple_property,
-    "patternProperties": _process_pattern_property,
-    "!parsing": _process_parsing_directive,
-    "$ref": _process_reference,
-    "!varname": _process_varname,
+    "properties": _process_simple_property_rule,
+    "unevaluatedProperties": _process_simple_property_rule,
+    "additionalProperties": _process_simple_property_rule,
+    "patternProperties": _process_pattern_property_rule,
+    "!parsing": _process_parsing_directive_rule,
+    "$ref": _process_reference_rule,
+    "!varname": _process_varname_rule,
 }

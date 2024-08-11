b999f34c678a7e154a01a340d43a74af328d91bd
JoseJVS <villamar_jose@hotmail.com>
2024-08-11 18:06:06 +0200

    Added documentation

--- src/metadata_archivist/InterpretationRules.py
+++ src/metadata_archivist/InterpretationRules.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+
 Interpretation rules defined for the SchemaInterpreter
 Rules can be customized or added through
 the INTERPRETATION_RULES dictionary.
 
 All rule functions must have the same arguments.
+
+Arguments:
+    interpreter: instance of schema interpreter. Used to continue interpretation on generated entry.
+    prop_val: value of property on which rule was called.
+    prop_key: key of property on which rule was called.
+    parent_key: key of parent property where current property is.
+    entry: schema entry where property is being interpreted.
+
+Returns:
+    interpreted schema entry.
+
+Only for internal use.
 
 Authors: Jose V., Matthias K.
 
@@ -15,9 +28,9 @@ from re import sub
 from copy import deepcopy
 from typing import Optional, Union
 
-from .Logger import LOG
+from .Logger import _LOG
 from .helper_functions import _math_check
-from .SchemaInterpreter import SchemaInterpreter, SchemaEntry
+from .SchemaInterpreter import _SchemaInterpreter, _SchemaEntry
 
 """
 Constants for schema specific/special values to be considered when parsing.
@@ -26,24 +39,24 @@ _KNOWN_REFS = [
     "#/$defs/",
 ]
 
-def _interpret_simple_property_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_simple_property_rule(interpreter: _SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # Known simple properties return recursion results without branching
     return interpreter._interpret_schema(prop_val, parent_key, entry)
 
-def _interpret_pattern_property_rule(interpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_pattern_property_rule(interpreter: _SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # We create a regex context and recurse overfrom copy import deepcopy the contents of the property.
     entry.context.update({"useRegex": True})
 
     return interpreter._interpret_schema(prop_val, parent_key, entry)
 
-def _interpret_parsing_directive_rule(interpreter: SchemaInterpreter, prop_val: Union[str, dict], prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_parsing_directive_rule(interpreter: _SchemaInterpreter, prop_val: Union[str, dict], prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # We create an !parsing context but keep on with current recursion level
     # Contents of this dictionary are not supposed to be handled by the interpreter.
     entry.context.update({prop_key: prop_val})
 
     return entry 
 
-def _interpret_varname_directive_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_varname_directive_rule(interpreter: _SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # Check if regex context is present in current entry
     if "useRegex" not in entry.context:
         raise RuntimeError("Contextless !varname found")
@@ -53,7 +66,7 @@ def _interpret_varname_directive_rule(interpreter: SchemaInterpreter, prop_val: 
     
     return entry
     
-def _interpret_reference_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_reference_rule(interpreter: _SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # Check if reference is well formed against knowledge base
     if not any(prop_val.startswith(ss) for ss in _KNOWN_REFS):
         raise RuntimeError(f"Malformed reference prop_value: {prop_key}: {prop_val}")
@@ -66,7 +79,18 @@ def _interpret_reference_rule(interpreter: SchemaInterpreter, prop_val: dict, pr
 
     return entry
 
-def __interpret_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> SchemaEntry:
+def __interpret_refs(definitions: dict, prop_val: str, entry: _SchemaEntry) -> _SchemaEntry:
+    """
+    Auxiliary function to check reference to parser.
+
+    Arguments:
+        definitions: schema definitions dictionary.
+        prop_val: string value of property where reference is invoked. Should correspond to parser name.
+        entry: schema entry where Parser was referenced.
+
+    Returns:
+        schema entry containing reference information.
+    """
     # Test correctness of reference
     val_split = prop_val.split("/")
     if len(val_split) < 3:
@@ -78,7 +102,7 @@ def __interpret_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> Sc
         raise KeyError(f"Parser not found: {pid}")
     
     # Add identified Parser to context
-    LOG.debug(f"processing reference to: {pid}")
+    _LOG.debug(f"processing reference to: {pid}")
     entry["!parser_id"] = pid
 
     # Further process reference e.g. filters, internal property references -> links
@@ -88,15 +112,29 @@ def __interpret_refs(definitions: dict, prop_val: str, entry: SchemaEntry) -> Sc
 
     return entry
 
-def __interpret_sub_schema(sub_schema: dict, entry: SchemaEntry, filters: Optional[list] = None) -> list:
-    # TODO: recursively explore Parser sub-schema and collect entry names and description.
-    # List of keys can be passed as filters to select a branch of the Parser sub schema.
-    # Resulting list should contain all the #/property that needs to be linked
-    # with their respective positions so that _process_refs can take care of creating links
-    # and avoid circular dependencies.
+def __interpret_sub_schema(sub_schema: dict, entry: _SchemaEntry, filters: Optional[list] = None) -> list:
+    """
+    WIP
+    Recursively explore Parser sub-schema and collect entry names and description.
+    List of keys can be passed as filters to select a branch of the Parser sub schema.
+    Resulting list should contain all the #/property that needs to be linked
+    with their respective positions so that _process_refs can take care of creating links
+    and avoid circular dependencies.
+
+    Arguments:
+        sub_schema: dictionary of Parser schema.
+        entry: schema entry where Parser was referenced.
+        filters: list of filters to apply to Parser schema.
+
+    Returns:
+        list of filtered sub-properties of Parser schema.
+    """
+
+    # TODO
+
     return []
 
-def _interpret_calculate_directive_rule(interpreter: SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: SchemaEntry) -> SchemaEntry:
+def _interpret_calculate_directive_rule(interpreter: _SchemaInterpreter, prop_val: dict, prop_key: str, parent_key: str, entry: _SchemaEntry) -> _SchemaEntry:
     # Calculates simple math expressions using values from parsers.
     # Requires referenced parsers to return numerical values.
     # references can be supplemented with !parsing directives to properly select value.
@@ -133,7 +171,7 @@ def _interpret_calculate_directive_rule(interpreter: SchemaInterpreter, prop_val
             raise RuntimeError(f"Variable does not reference a parser in !calculate directive: {variable}={value}")
         
         # We create a SchemaEntry in the context to be specially handled by the Formatter
-        new_entry = SchemaEntry(key=prop_key, context=deepcopy(entry.context))
+        new_entry = _SchemaEntry(key=prop_key, context=deepcopy(entry.context))
 
         if "!parsing" in value:
            _interpret_parsing_directive_rule(interpreter, value["!parsing"], "!parsing", prop_key, new_entry)

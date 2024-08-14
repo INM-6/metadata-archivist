f997e53bad3102f272565eaf2da22f7623f5fae7
JoseJVS <villamar_jose@hotmail.com>
2024-08-14 14:15:35 +0200

    Updated schema example 3

--- examples/schema_example3/main.py
+++ examples/schema_example3/main.py
@@ -1,32 +1,99 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+
+Minimal orchestration of metadata parsing, formatting, and exporting example.
+Uses custom schema to do patternProperties with nested properties structuring of output.
+
+Requires PyYAML.
+
+Authors: Matthias K., Jose V.
+
+"""
 
 from metadata_archivist import Archivist
-from my_parser import my_parser
+from my_parsers import time_parser, yml_parser, station_character_parser, basin_character_parser
 from pathlib import Path
 from json import dumps, dump
-import shutil
 
-tmpdir = Path('tmp')
-if tmpdir.exists():
-    shutil.rmtree(tmpdir)
 
-arch = Archivist(path=Path('metadata_archive.tar'),
-                 extraction_directory='tmp',
-                 formatter=my_parser,
-                 output_directory="./",
-                 output_file="metadata.json",
-                 overwrite=True,
-                 auto_cleanup=True,
-                 verbose='debug')
+my_schema = {
+    '$schema': 'https://abc',
+    '$id': 'https://abc.json',
+    'description': 'my example schema 3',
+    'type': 'object',
+    'properties': {
+        'metadata_archive': {
+            'type': 'object',
+            'patternProperties': {
+                "^basin_.*": {
+                    "type": "object",
+                    "properties": {
+                        "basin_characteristics": {
+                            "$ref": "#/$defs/basin_character_parser"
+                        },
+                        "simulation": {
+                            'type': 'object',
+                            'properties': {
+                                'time_info': {
+                                    '$ref': '#/$defs/time_parser'
+                                },
+                                'model_configuration': {
+                                    '$ref': '#/$defs/yml_parser'
+                                },
+                            }
+                        }
+                    }
+                },
+                "^station_.*": {
+                    "type": "object",
+                    "properties": {
+                        "basin_characteristics": {
+                            "$ref": "#/$defs/station_character_parser"
+                        },
+                        "simulation": {
+                            'type': 'object',
+                            'properties': {
+                                'time_info': {
+                                    '$ref': '#/$defs/time_parser'
+                                },
+                                'model_configuration': {
+                                    '$ref': '#/$defs/yml_parser'
+                                },
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    }
+}
 
-arch.parse()
-arch.export()
 
-print("\nResulting schema:")
-print(dumps(my_parser.schema, indent=4))
-with Path("schema.json").open("w") as f:
-    dump(my_parser.schema, f, indent=4)
+if __name__ == "__main__":
+    arch = Archivist(path='metadata_archive.tar',
+                    parsers=[
+                        time_parser(),
+                        yml_parser(),
+                        station_character_parser(),
+                        basin_character_parser()
+                        ],
+                    schema=my_schema,
+                    extraction_directory='tmp',
+                    output_directory="./",
+                    output_file="metadata.json",
+                    overwrite=True,
+                    auto_cleanup=True,
+                    verbose='info')
 
-print("\nResulting metadata:")
-print(dumps(arch.get_metadata(), indent=4))
+    arch.parse()
+    arch.export()
+
+    print("\nResulting schema:")
+    formatted_schema = arch.get_formatted_schema()
+    print(dumps(formatted_schema, indent=4))
+    with Path("schema.json").open("w") as f:
+        dump(formatted_schema, f, indent=4)
+
+    print("\nResulting metadata:")
+    print(dumps(arch.get_metadata(), indent=4))
--- examples/schema_example3/my_parsers.py
+++ examples/schema_example3/my_parsers.py
@@ -1,4 +1,13 @@
-from metadata_archivist import AParser, Formatter
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Parsers instances examples with schema.
+Authors: Matthias K., Jose V.
+
+"""
+
+from metadata_archivist import AParser
 import yaml
 
 
@@ -166,64 +175,3 @@ class station_character_parser(AParser):
                 return out
             except yaml.YAMLError as exc:
                 print(exc)
-
-
-my_schema = {
-    '$schema': 'https://abc',
-    '$id': 'https://abc.json',
-    'description': 'my example schema 3',
-    'type': 'object',
-    'properties': {
-        'metadata_archive': {
-            'type': 'object',
-            'patternProperties': {
-                "^basin_.*": {
-                    "type": "object",
-                    "properties": {
-                        "basin_characteristics": {
-                            "$ref": "#/$defs/basin_character_parser"
-                        },
-                        "simulation": {
-                            'type': 'object',
-                            'properties': {
-                                'time_info': {
-                                    '$ref': '#/$defs/time_parser'
-                                },
-                                'model_configuration': {
-                                    '$ref': '#/$defs/yml_parser'
-                                },
-                            }
-                        }
-                    }
-                },
-                "^station_.*": {
-                    "type": "object",
-                    "properties": {
-                        "basin_characteristics": {
-                            "$ref": "#/$defs/station_character_parser"
-                        },
-                        "simulation": {
-                            'type': 'object',
-                            'properties': {
-                                'time_info': {
-                                    '$ref': '#/$defs/time_parser'
-                                },
-                                'model_configuration': {
-                                    '$ref': '#/$defs/yml_parser'
-                                },
-                            }
-                        }
-                    }
-                }
-            }
-        },
-    }
-}
-
-my_parser = Formatter(parsers=[
-    time_parser(),
-    yml_parser(),
-    station_character_parser(),
-    basin_character_parser()
-],
-                   schema=my_schema)
